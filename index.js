cf5f3f96f991cb17e5a9f882f76521f67c1b4181
Javier Silva <JAVSILVAS123@GMAIL.COM>
2022-03-10 22:32:58 -0500

    Update index.js

--- index.js
+++ index.js
@@ -41,6 +41,7 @@ client.once('ready', () => {
 
 //Command Event Listener
 client.on("message", async message => {
+    console.log(message);
     if(message.author.bot) return;
     if(message.channel.type === 'dm') return;
     if(message.content.startsWith(prefix))  {
